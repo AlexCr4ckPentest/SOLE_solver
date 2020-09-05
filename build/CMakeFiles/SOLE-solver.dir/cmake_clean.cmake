8bb7827156e81e468e767155e712c54671b21fcc
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-09-05 20:41:11 +0300

    upload sr files

---
+++ build/CMakeFiles/SOLE-solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SOLE-solver.dir/src/main.cpp.o"
+  "SOLE-solver"
+  "SOLE-solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SOLE-solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
